c73adf32e0b417a6fc3cb6afc867c4e5a6e22104
KEEPITIL <illestratedlifestyle@gmail.com>
2026-06-27 05:25:47 -0400

    SW: clear caches and auto-reload all clients

--- sw.js
+++ sw.js
@@ -4,5 +4,7 @@ self.addEventListener('activate', event => {
     caches.keys()
       .then(names => Promise.all(names.map(n => caches.delete(n))))
       .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll({type:'window'}))
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
 });
