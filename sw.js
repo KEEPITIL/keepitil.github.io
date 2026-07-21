5b713e6592771661be5c05ddfd9bd9658c1a23dd
KEEPITIL <illestratedlifestyle@gmail.com>
2026-07-21 10:56:37 -0700

    KEEPITIL batch deploy 2026-07-21 10:55

--- sw.js
+++ sw.js
@@ -7,10 +7,10 @@
    - Only images/fonts/icons keep stale-while-revalidate (stale pixels can't break logic).
    - VERSION bump evicts every v1/v2 cache on activate + clients.claim().
    To retire this SW: bump VERSION and ship, or restore the self-destruct worker. */
-var VERSION = 'kil-pwa-v27-20260721b';
-var PAGES = 'kil-pages-v27';
-var ASSETS = 'kil-assets-v27';
-var CODE = 'kil-code-v27';
+var VERSION = 'kil-pwa-v28-20260721c';
+var PAGES = 'kil-pages-v28';
+var ASSETS = 'kil-assets-v28';
+var CODE = 'kil-code-v28';
 var KEEP = [VERSION, PAGES, ASSETS, CODE];
 var PAGE_LIMIT = 40;
 var PRECACHE = [
