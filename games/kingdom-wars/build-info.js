b3d7203364351a77a2f5d3d3d67c84e4e7c6e7f2
KEEPITIL <illestratedlifestyle@gmail.com>
2026-07-23 21:08:30 -0700

    Kingdom Wars Build 30: animated march cadence, brisker pace

--- games/kingdom-wars/build-info.js
+++ games/kingdom-wars/build-info.js
@@ -1,4 +1,4 @@
 (function(){
   'use strict';
-  window.KWBuild=Object.freeze({appVersion:'0.18.0-alpha',buildNumber:29,commitHash:'local-build27',contentVersion:'2026.07.23.03',saveSchemaVersion:4,analyticsSchemaVersion:1,environment:'development',modeId:'UNIVERSAL_BASE_SOLDIER_RIG'});
+  window.KWBuild=Object.freeze({appVersion:'0.18.0-alpha',buildNumber:30,commitHash:'local-build27',contentVersion:'2026.07.23.04',saveSchemaVersion:4,analyticsSchemaVersion:1,environment:'development',modeId:'UNIVERSAL_BASE_SOLDIER_RIG'});
 })();
