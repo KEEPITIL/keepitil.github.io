1ba05f454cbf080e1806ef4433857db2667d0f4d
KEEPITIL <illestratedlifestyle@gmail.com>
2026-07-26 11:34:13 -0700

    Move Kingdom Wars to /games/ageofwars (redirect from old path)

---
+++ games/ageofwars/src/systems/build-info.js
@@ -0,0 +1,4 @@
+(function(){
+  'use strict';
+  window.KWBuild=Object.freeze({appVersion:'0.18.0-alpha',buildNumber:40,commitHash:'local-build27',contentVersion:'2026.07.25.01',saveSchemaVersion:4,analyticsSchemaVersion:1,environment:'development',modeId:'UNIVERSAL_BASE_SOLDIER_RIG'});
+})();
