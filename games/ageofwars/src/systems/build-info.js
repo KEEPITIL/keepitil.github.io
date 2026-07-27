7968dd4c10a1856dfd4ceba075e34e22ad62000f
KEEPITIL <illestratedlifestyle@gmail.com>
2026-07-27 01:11:14 -0700

    Age of Wars Build 47: giants + payments

--- games/ageofwars/src/systems/build-info.js
+++ games/ageofwars/src/systems/build-info.js
@@ -1,4 +1,4 @@
 (function(){
   'use strict';
-  window.KWBuild=Object.freeze({appVersion:'0.18.0-alpha',buildNumber:40,commitHash:'local-build27',contentVersion:'2026.07.25.01',saveSchemaVersion:4,analyticsSchemaVersion:1,environment:'development',modeId:'UNIVERSAL_BASE_SOLDIER_RIG'});
+  window.KWBuild=Object.freeze({appVersion:'0.18.0-alpha',buildNumber:47,commitHash:'local-build27',contentVersion:'2026.07.27.01',saveSchemaVersion:4,analyticsSchemaVersion:1,environment:'development',modeId:'UNIVERSAL_BASE_SOLDIER_RIG'});
 })();
